7b9ef5fb1ecefe8d5cd1ed61452d8ed70eb26b21
Roni Dover <roni.dover@gmail.com>
2020-02-17 11:22:14 -0800

    Update install_plugin.sh

--- install_plugin.sh
+++ install_plugin.sh
@@ -2,8 +2,8 @@
 set -e
 cat /var/log/syslog | grep -oP "(?<=password to ').*(?=')"
 cd /opt/bitnami/apps/magento/htdocs
-sudo sed 's/<public-key>/$PUBLIC_KEY/g' -i auth.json.sample
-sudo sed 's/<private-key>/$PRIVATE_KEY/g' -i auth.json.sample
+sudo sed "s/<public-key>/$PUBLIC_KEY/g" -i auth.json.sample
+sudo sed "s/<private-key>/$PRIVATE_KEY/g" -i auth.json.sample
 sudo cp auth.json.sample auth.json
 sudo composer require cloudinary/cloudinary:$PLUGIN_VERSION
 sudo ./bin/magento-cli module:enable Cloudinary_Cloudinary --clear-static-content
