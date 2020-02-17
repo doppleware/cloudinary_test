291116c8ecb7c7a7e1e4c05a66156d5ec5230fae
Roni Dover <roni.dover@gmail.com>
2020-02-17 09:14:45 -0800

    Update install_plugin.sh

--- install_plugin.sh
+++ install_plugin.sh
@@ -1,3 +1,6 @@
  cd /opt/bitnami/apps/magento/htdocs
+ sudo sed 's/<public-key>/$PUBLIC_KEY/g' -i auth.json.sample
+ sudo sed 's/<private-key>/$PRIVATE_KEY/g' -i auth.json.sample
+ sudo cp auth.json.sample auth.json
  sudo composer require cloudinary/cloudinary:$PLUGIN_VERSION
  cat /var/log/syslog | grep -oP "(?<=password to ').*(?=')"
