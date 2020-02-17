a20e8381e229786e5e8907cc7d17514069268e8d
Roni Dover <roni.dover@gmail.com>
2020-02-17 11:37:17 -0800

    Update install_plugin.sh

--- install_plugin.sh
+++ install_plugin.sh
@@ -5,8 +5,14 @@ cd /opt/bitnami/apps/magento/htdocs
 sudo sed "s/<public-key>/$PUBLIC_KEY/g" -i auth.json.sample
 sudo sed "s/<private-key>/$PRIVATE_KEY/g" -i auth.json.sample
 sudo cp auth.json.sample auth.json
+echo Updated keys: $?
 sudo composer require cloudinary/cloudinary:$PLUGIN_VERSION
+echo Installed module: $?
 sudo ./bin/magento-cli module:enable Cloudinary_Cloudinary --clear-static-content
+echo Enabled module: $?
 sudo ./bin/magento-cli setup:upgrade
+echo Setup upgrade: $?
 sudo ./bin/magento-cli setup:di:compile
+echo Setup compile: $?
 sudo ./bin/magento-cli cache:clean
+echo Clean cache: $?
