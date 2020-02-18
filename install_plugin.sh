062b3f1e8af3b44f5006aae440cb84f2203ddf38
Roni Dover <roni.dover@gmail.com>
2020-02-17 17:21:49 -0800

    Update install_plugin.sh

--- install_plugin.sh
+++ install_plugin.sh
@@ -4,22 +4,22 @@ cd /opt/bitnami/apps/magento/htdocs
 echo stage is: $Stage
 if [ "$Stage" = "Setup" ] 
 then 
-  echo doing setup
   sudo sed "s/<public-key>/$PUBLIC_KEY/g" -i auth.json.sample
   sudo sed "s/<private-key>/$PRIVATE_KEY/g" -i auth.json.sample
-  sudo cp auth.json.sample auth.json
+  sudo cp auth.json.sample auth.json  
+  sudo composer require cloudinary/cloudinary:$PLUGIN_VERSION
+  echo Installed cloudinary/cloudinary:"$PLUGIN_VERSION" 
 fi
 if [ "$Stage" = "Install" ]
 then 
-  echo doing install
-  sudo composer require cloudinary/cloudinary:$PLUGIN_VERSION
   sudo ./bin/magento-cli module:enable Cloudinary_Cloudinary --clear-static-content
+  echo Enabled plugin 
 fi
 if [ "$Stage" = "Refresh" ]
 then 
-  echo doing refresh!
   sudo ./bin/magento-cli setup:upgrade
   sudo ./bin/magento-cli setup:di:compile
   sudo ./bin/magento-cli cache:clean
+  echo Refreshed, Compiled and cleaned Cache
 fi
 
